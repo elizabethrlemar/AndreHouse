ea73a229663ca958785c23ea5f14df6c638bd3aa
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2022-02-02 14:50:27 -0600

    Added two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Icon Tutorial',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -26,22 +27,45 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Andre House'),
+        title: const Text('Andre House'),
       ),
       body: Column(children: <Widget>[
+        Container(
+        padding: const EdgeInsets.all(20),
+        alignment: Alignment.center,
+        child: Column(
+          children: const <Widget>[
+            Icon(
+              Icons.shower,
+              size: 150,
+              color: Colors.blue,
+            ),
+            Text(
+              'Shower',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),),
+
         //icon with label below it
         Container(
-          padding: EdgeInsets.all(30),
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.center,
           child: Column(
-            children: <Widget>[
-              Icon(Icons.shower, color:Colors.blue, size: 150),
-              Text('Shower', style: TextStyle(fontSize: 25)),
+            children: const <Widget>[
+              Icon(
+                Icons.checkroom,
+                size: 150,
+                color: Colors.pink,
+              ),
+              Text(
+                'Clothing Closet',
+                style: TextStyle(fontSize: 20),
+              ),
             ],
           ),
         )
       ]),
     );
   }
-
-
 }
