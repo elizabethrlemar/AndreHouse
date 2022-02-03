d68e29e6ac172a597d4db86a0c99e05ba7e565f3
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-02-03 16:14:28 -0600

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,122 @@
+
 import 'package:flutter/material.dart';
-import 'guest_homepage.dart';
-void main() => runApp(MyApp());
+import 'package:loginpage/UI/input_field.dart';
+import 'package:loginpage/home_page.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Icon Tutorial',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return const MaterialApp(
+      //Material App
+      debugShowCheckedModeBanner: false,
+      title: "Login App",
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  //StateFullWidget
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Scaffold
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.lightBlueAccent,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomRight,
+              heightFactor: 0.5,
+              widthFactor: 0.5,
+              child: Material(
+                borderRadius: const BorderRadius.all(Radius.circular(200.0)),
+                color: const Color.fromRGBO(255, 255, 255, 0.4),
+                child: Container(
+                  width: 400,
+                  height: 400,
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                width: 400,
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Material(
+                        elevation: 10.0,
+                        borderRadius:
+                        const BorderRadius.all(Radius.circular(50.0)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            "images/flutter-logo.png",
+                            width: 80,
+                            height: 80,
+                          ),
+                        )),
+                    Form(
+                      child: InputField(
+                        //Calling inputField  class
+
+                          const Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          "Username"),
+                    ),
+                    Form(
+                      child: InputField(
+                          const Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          "Password"),
+                    ),
+                    Container(
+                      width: 150,
+                      child: RaisedButton(
+                        //Raised Button
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => MyHomePage()));
+                        },
+                        color: Colors.indigo,
+                        textColor: Colors.white,
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
