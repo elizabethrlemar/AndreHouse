06ce69e20b06ac51a7706e9adc3e9302dd3af24b
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-02-17 23:35:17 -0600

    working on authentication

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,19 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService{
+class AuthService {
 
-  final FirebaseAuth _auth=FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //sign in anon
-  Future signInAnon() async{
+  // sign in anon
+  Future signInAnon() async {
     try {
-      AuthResult result=await _auth.signInAnonymously();
-      FirebaseUser user = result.user;
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: "barry.allen@example.com",
+          password: "SuperSecretPassword!"
+      );
+      final User? user = userCredential.user;
       return user;
-    }catch(e){
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
+
+// sign in with email and password
+
+
+// register with email and password
+
+// sign out
 
 }
