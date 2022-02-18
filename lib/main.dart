dea7a576980b9b75d6dfab402722290385faa604
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-02-17 22:11:56 -0600

    added WidgetsFlutterBinding to initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async=> runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}=> runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
