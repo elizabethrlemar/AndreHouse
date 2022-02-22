6aa98cc8a670e5d84bc1b309576e7f34cc46eae7
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2022-02-21 18:01:33 -0600

    added anonymous button to login page

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       //Material App
       debugShowCheckedModeBanner: false,
       title: "Login App",
-      home: MyStaffHomePage(),
+      home: HomeScreen(),
     );
   }
 }
