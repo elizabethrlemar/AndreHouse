a29b2806d946b032ac64cd5bf0cd765503779f2c
John <johnmc9605@gmail.com>
2022-02-27 13:17:09 -0600

    Working on staff shower page

--- lib/StaffPages/staff_shower.dart
+++ lib/StaffPages/staff_shower.dart
@@ -14,12 +14,48 @@ class _ShowerState extends State<StaffShowerPage> {
         title: const Text('Shower'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
+          child: GridView.count(
+              scrollDirection: Axis.vertical,
+              crossAxisCount: 2,
+              children: <Widget>[
+                Expanded(
+                    child: Column(
+                        children: <Widget>[
+                          Expanded(
+                              child: IconButton(
+                                icon: const Icon(Icons.shower),
+                                iconSize: 100,
+                                color: Colors.blue,
+                                onPressed: () {},
+                              )
+                          ),
+                          const Text(
+                            'View Line',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]
+                    )
+                ),
+                Expanded(
+                    child: Column(
+                        children: <Widget>[
+                          Expanded(
+                              child: IconButton(
+                                icon: const Icon(Icons.numbers),
+                                iconSize: 100,
+                                color: Colors.red,
+                                onPressed: () {},
+                              )
+                          ),
+                          const Text(
+                            "Manage Line",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]
+                    )
+                ),
+              ]
+          )
       ),
     );
   }
