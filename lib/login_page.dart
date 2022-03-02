74b68093316b4cf678e0040c00c7117c3643403d
Elizabeth LeMar <elizabethrlemar@gmail.com>
2022-03-02 16:01:57 -0600

    finished updated login page and added image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,120 +1,156 @@
-import 'package:andre_house/GuestPages/guest_signup.dart';
 import 'package:flutter/material.dart';
+import 'package:andre_house/GuestPages/guest_signup.dart';
 import 'package:andre_house/UI/input_field.dart';
 import 'package:andre_house/StaffPages/staff_homepage.dart';
 import 'package:andre_house/GuestPages/guest_homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      //Material App
-      debugShowCheckedModeBanner: false,
-      title: "Login App",
-      home: HomeScreen(),
-    );
-  }
-}
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super (key: key);
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-  //StateFullWidget
   @override
   HomeScreenState createState() => HomeScreenState();
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class HomeScreenState extends State<HomeScreen>{
+
+  final formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    final emailForm = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: ,
+      onSaved: (value){
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        filled: true,
+        prefixIcon: Icon(Icons.person),
+        contentPadding: const EdgeInsets.fromLTRB(10, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ));
+
+    final passwordForm = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      //validator: ,
+      onSaved: (value){
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          prefixIcon: const Icon(Icons.lock),
+          contentPadding: const EdgeInsets.fromLTRB(10, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    final loginButton = Material(
+        color: Colors.transparent,
+        child: Material(
+          elevation: 5,
+          color: Colors.indigo,
+          borderRadius: BorderRadius.circular(10),
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            onPressed: () {},
+            child: const Text("Login", style: TextStyle(fontSize: 20)),
+            textColor: Colors.white,
+          ),
+        ));
+
     return Scaffold(
       //Scaffold
+      backgroundColor: Colors.indigo,
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.lightBlueAccent,
-        child: Stack(
+          child: Stack(
           children: <Widget>[
-            Align(
+            const Align(
               alignment: Alignment.bottomRight,
               heightFactor: 0.5,
               widthFactor: 0.5,
               child: Material(
-                borderRadius: const BorderRadius.all(Radius.circular(200.0)),
-                color: const Color.fromRGBO(255, 255, 255, 0.4),
-                child: Container(
+                borderRadius: BorderRadius.all(Radius.circular(200.0)),
+                color: Color.fromRGBO(255, 255, 255, 0.4),
+                child: SizedBox(
                   width: 400,
                   height: 400,
                 ),
               ),
             ),
             Center(
-              child: Container(
+              child: SizedBox(
                 width: 400,
                 height: 400,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Text("Andre House", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),),
-                    Form(
-                      child: InputField(
-                        //Calling inputField  class
-
-                          const Icon(
-                            Icons.person,
-                            color: Colors.white,
-                          ),
-                          "Username"),
-                    ),
-                    Form(
-                      child: InputField(
-                          const Icon(
-                            Icons.lock,
-                            color: Colors.white,
-                          ),
-                          "Password"),
-                    ),
-                    Container(
-                      width: 150,
-                      child: RaisedButton(
-                        //Raised Button
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => MyHomePage()));
-                        },
-                        color: Colors.indigo,
-                        textColor: Colors.white,
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
+                    SizedBox(
+                      width: 300,
+                      child:
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 75,
+                              child: Image.asset("images/logo.png",
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            const SizedBox(height: 25),
+                            emailForm,
+                            const SizedBox(height: 8),
+                            passwordForm,
+                            const SizedBox(height: 25),
+                            loginButton,
+                            const SizedBox(height: 25),
+                            Row(
+                              children: <Widget> [
+                                const Text("Don't have an account?", style: TextStyle(fontSize: 20, color: Colors.indigo),),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (context) => const SignUpScreen()));
+                                  },
+                                  child: const Text(
+                                    " Sign Up!",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                )
+                              ],
+                              mainAxisAlignment: MainAxisAlignment.center,
+                            ),
+                            const SizedBox(height: 25),
+                          ]
                         ),
                       ),
                     ),
-                    Container(
-                      child:
-                        Row(
-                          children: <Widget> [
-                            const Text ("Don't have an account?", style: TextStyle(fontSize: 20)),
-                            TextButton(
-                              child: const Text(
-                                "Sign Up!",
-                                style: TextStyle(fontSize: 20, color: Colors.white),
-                              ),
-                              onPressed: (){
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => SignUpScreen()));
-                              },
-                            ),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.center,
-                        )
-                    ),
-                    Container(
+                    SizedBox(
                       width: 200,
                       child: RaisedButton(
                         //Raised Button
