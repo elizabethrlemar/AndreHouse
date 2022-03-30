66e1811d7f6044845eacb12bdf37ea95f2d18bc1
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-03-29 21:03:07 -0500

    added homepage class for admin_homepage.dart

--- lib/AdminPages/admin_homepage.dart
+++ lib/AdminPages/admin_homepage.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 import 'guest_shower.dart';
 import 'guest_clothing.dart';
 import 'help_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
