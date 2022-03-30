900fc3e33b57f5005b0faa014aedc57b54e3945c
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-03-29 21:09:19 -0500

    added shower page for admin

---
+++ lib/AdminPages/admin_shower.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+/*Page that allows guests to sign up for showers*/
+class GuestShowerPage extends StatefulWidget {
+  const GuestShowerPage({Key? key}) : super(key: key);
+  @override
+  _ShowerState createState() => _ShowerState();
+}
