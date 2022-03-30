7a9c482326c9cc30d5cb1eb5296c509397ba48d6
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-03-29 21:08:35 -0500

    added clothing state for admin

--- lib/AdminPages/admin_clothing.dart
+++ lib/AdminPages/admin_clothing.dart
@@ -5,3 +5,52 @@ class GuestClothingPage extends StatefulWidget {
   @override
   _ClothingState createState() => _ClothingState();
 }
+
+class _ClothingState extends State<GuestClothingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Clothing Closet'),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+          child: GridView.count(
+              scrollDirection: Axis.vertical,
+              crossAxisCount: 2,
+              children: <Widget>[
+                Expanded(
+                    child: Column(
+                        children: <Widget>[
+                          Expanded(
+                              child: IconButton(
+                                icon: const Icon(Icons.checkroom),
+                                iconSize: 100,
+                                color: Colors.red,
+                                onPressed: () {},
+                              )
+                          ),
+                          const Text(
+                            'Sign-Up',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]
+                    )
+                ),
+                Expanded(
+                    child: Column(
+                        children: <Widget>[
+                          const Text(
+                            "My Spot in Line",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ]
+                    )
+                ),
+              ]
+          )
+      ),
+    );
+  }
+}
