55af5cc115ddf4955ae55231f5d9fc64178f2718
edeguzman23 <ethan-deguzman@uiowa.edu>
2022-03-29 21:08:06 -0500

    added page for clothing for admin

---
+++ lib/AdminPages/admin_clothing.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+/* Page that allows guests to sign up for Clothing Closet*/
+class GuestClothingPage extends StatefulWidget {
+  const GuestClothingPage({Key? key}) : super(key: key);
+  @override
+  _ClothingState createState() => _ClothingState();
+}
